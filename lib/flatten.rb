0e91d86bd929f19ab75083ea4a8592d13386ce53
daneb <dane.balia@hetzner.co.za>
2017-04-21 12:58:28 +0200

    added more uses cases and implementation

--- lib/flatten.rb
+++ lib/flatten.rb
@@ -1,6 +1,11 @@
 require "flatten/version"
 
 module Flatten
-  def flatten_array(array)
+  class Error < StandardError; end
+
+  def to_flat(array, result = [])
+    return result if array.length.zero?
+    array[0].is_a?(Array) ? to_flat(array[0], result) : result << array[0]
+    to_flat(array.drop(1), result)
   end
 end
--- spec/flatten_spec.rb
+++ spec/flatten_spec.rb
@@ -1,19 +1,40 @@
-require "spec_helper"
+require 'spec_helper'
 
 RSpec.describe Flatten do
-  let(:fl) { Class.new { extend Flatten }}
+  let(:fl) { Class.new { extend Flatten } }
 
   before :all do
-    
   end
 
-  it "has a version number" do
+  it 'has a version number' do
     expect(Flatten::VERSION).not_to be nil
   end
 
-  it "flattens a simple nested array" do
-    simple_array = [1, [2, [3]], 4]
-    result = [1, 2, 3, 4]
-    expect(fl.flatten_array(simple_array)).to eq(result)
+  describe 'use cases' do
+    it 'flattens a simple nested array' do
+      simple_array = [1, [2, [3]], 4]
+      result = [1, 2, 3, 4]
+      expect(fl.to_flat(simple_array)).to eq(result)
+    end
+
+    it 'flattens a deeply nested array' do
+      complex_array = [1, [1, 2, 3, [4, 5, 6, [7, 8, 9]]]]
+      result = [1, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+      expect(fl.to_flat(complex_array)).to eq(result)
+    end
+
+    it 'flattens multiple deeply nested arrays' do
+      multiples = [1, 2, 3, [4, 5, 6, [7, 8, 9], [10, 11, 12, [13, 14, 15, 16]]]]
+      result = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
+      expect(fl.to_flat(multiples)).to eq(result)
+    end
+  end
+  
+  describe 'error handling' do
+    it 'catches unhandled exceptions' do
+    end
+  end
+  
+  describe 'non-functional requirements' do
   end
 end
